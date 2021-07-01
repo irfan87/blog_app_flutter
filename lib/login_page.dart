7e4aaa10ee07613781c4d674a9d236cc55ea2576
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-07-01 23:41:06 +0800

    removed the scaffold to the login page

---
+++ lib/login_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "welcome to the blog app".toUpperCase(),
+                  style: TextStyle(
+                    letterSpacing: 3.0,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                  ),
+                ),
+                Image.asset("images/welcome.png"),
+                Column(
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "login".toUpperCase(),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blueGrey[900],
+                        minimumSize: Size(250.0, 45.0),
+                        elevation: 0.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "register".toUpperCase(),
+                      ),
+                      style: TextButton.styleFrom(
+                        backgroundColor: Colors.blueGrey[100],
+                        primary: Colors.blueGrey[900],
+                        minimumSize: Size(250.0, 45.0),
+                        elevation: 0.0,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
